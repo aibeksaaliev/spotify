b9ccf9c7aa709ce2c1286e3fcd7c1a9998d5b5b0
Aibek Saaliev <dokaidea@gmail.com>
2023-02-22 16:50:02 +0600

    added static middleware for public directory

--- back/index.ts
+++ back/index.ts
@@ -13,6 +13,7 @@ const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
