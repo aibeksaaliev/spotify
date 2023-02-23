52dda6e4f14de66b53809cfd90b951024fe86368
Aibek Saaliev <dokaidea@gmail.com>
2023-02-23 17:24:04 +0600

    added theme in layout component

--- front/src/components/Layout/Layout.tsx
+++ front/src/components/Layout/Layout.tsx
@@ -10,7 +10,20 @@ import {Link} from "react-router-dom";
 
 const Layout: React.FC<PropsWithChildren> = ({children}) => {
 
-  const theme = createTheme();
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#424242',
+      },
+      secondary: {
+        light: '#0066ff',
+        main: '#0044ff',
+        contrastText: '#ffcc00',
+      },
+      contrastThreshold: 3,
+      tonalOffset: 0.2,
+    },
+  });
 
   return (
     <ThemeProvider theme={theme}>
