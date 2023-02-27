ec2dca9097377f8230b0501af6741436223b8d48
Aibek Saaliev <dokaidea@gmail.com>
2023-02-27 15:43:25 +0600

    HW#86 - added opportunity to submit track history

--- front/src/components/Cards/TrackCard.tsx
+++ front/src/components/Cards/TrackCard.tsx
@@ -3,15 +3,21 @@ import {Button, ListItem, ListItemIcon, ListItemText} from "@mui/material";
 import AudiotrackIcon from '@mui/icons-material/Audiotrack';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import {TrackType} from "../../types";
-import {useAppSelector} from "../../app/hooks";
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {selectUser} from "../../feauters/users/usersSlice";
+import {submitTrackHistory} from "../../feauters/tracks/tracksThunks";
 
 interface Props {
   track: TrackType;
 }
 
 const TrackCard: React.FC<Props> = ({track}) => {
+  const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
+
+  const playSong = async () => {
+    await dispatch(submitTrackHistory(track._id));
+  };
 
   return (
     <ListItem>
@@ -26,7 +32,7 @@ const TrackCard: React.FC<Props> = ({track}) => {
       </ListItemText>
       <ListItemText sx={{textAlign: "right"}}>
         {user && (
-          <Button>
+          <Button onClick={playSong}>
             <PlayCircleOutlineIcon/>
           </Button>
         )}
