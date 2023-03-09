ea15ffce2c7ad673818801bea6c10e4f1be67af3
Aibek Saaliev <dokaidea@gmail.com>
2023-03-09 16:54:25 +0600

    HW#89 - added protected route component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,15 +8,18 @@ import Error from "./components/Error/Error";
 import Register from "./containers/Users/Register";
 import Login from "./containers/Users/Login";
 import TrackHistory from "./containers/TrackHistory";
-import {useAppDispatch} from "./app/hooks";
+import {useAppDispatch, useAppSelector} from "./app/hooks";
 import {clearYouTubeUrl} from "./feauters/tracks/tracksSlice";
 import NewArtist from "./containers/Artists/NewArtist";
 import NewAlbum from "./containers/Albums/NewAlbum";
 import NewTrack from "./containers/Tracks/NewTrack";
+import {selectUser} from "./feauters/users/usersSlice";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   const dispatch = useAppDispatch();
   const location = useLocation();
+  const user = useAppSelector(selectUser);
 
   if (location.pathname !== "/albums:id") {
     dispatch(clearYouTubeUrl());
@@ -28,10 +31,22 @@ function App() {
         <Routes>
           <Route path="/" element={<Artists/>}/>
           <Route path="/artists/:id" element={<Albums/>}/>
-          <Route path="/add_new_artist" element={<NewArtist/>}/>
+          <Route path="/add_new_artist" element={(
+            <ProtectedRoute isAllowed={!!user}>
+              <NewArtist/>
+            </ProtectedRoute>
+          )}/>
           <Route path="/albums/:id" element={<Tracks/>}/>
-          <Route path="/add_new_album" element={<NewAlbum/>}/>
-          <Route path="/add_new_track" element={<NewTrack/>}/>
+          <Route path="/add_new_album" element={(
+            <ProtectedRoute isAllowed={!!user}>
+              <NewAlbum/>
+            </ProtectedRoute>
+          )}/>
+          <Route path="/add_new_track" element={(
+            <ProtectedRoute isAllowed={!!user}>
+              <NewTrack/>
+            </ProtectedRoute>
+          )}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/track_history" element={<TrackHistory/>}/>
--- front/src/types.d.ts
+++ front/src/types.d.ts
@@ -69,6 +69,7 @@ export interface User {
   _id: string;
   username: string;
   token: string;
+  role: string;
 }
 
 export interface RegisterResponse {
